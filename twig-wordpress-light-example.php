b716c353114f8606d5c3fa1b9cbbcccf9ea21947
Guido Scialfa <dev@guidoscialfa.com>
2018-12-02 20:51:08 +0100

    Introduce main plugin file

--- twig-wordpress-light-example.php
+++ twig-wordpress-light-example.php
@@ -1,5 +1,12 @@
 <?php # -*- coding: utf-8 -*-
-declare(strict_types=1);
+
+/**
+ * Plugin Name: Twig WordPress Light Example
+ * Author: Guido Scialfa
+ * Author URI: guidoscialfa.com
+ * Description: A light example of Twig for WordPress
+ * Version: 0.1
+ */
 
 /*
  * This file is part of the twig-wordpress-light-example package.
@@ -9,3 +16,9 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
+
+add_action('plugins_loaded', function () {
+
+});
