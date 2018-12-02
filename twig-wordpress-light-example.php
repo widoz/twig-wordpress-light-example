8854c6c6f5cfc0e5f94a276dca2fd1666decba59
Guido Scialfa <dev@guidoscialfa.com>
2018-12-02 19:14:03 +0100

    Update readme

---
+++ twig-wordpress-light-example.php
@@ -0,0 +1,11 @@
+<?php # -*- coding: utf-8 -*-
+declare(strict_types=1);
+
+/*
+ * This file is part of the twig-wordpress-light-example package.
+ *
+ * (c) Guido Scialfa <dev@guidoscialfa.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
